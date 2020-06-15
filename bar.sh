26f3acbdbcdcce4b3bfd73fa34c3f8b8261b378f
tajo48 <55502906+tajo48@users.noreply.github.com>
2020-06-16 01:57:29 +0200

    Update bar.sh

--- bar.sh
+++ bar.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 while :
 do
-	xsetroot -name "$( date +"%F %R" )"
+
+
+for battery in /sys/class/power_supply/BAT?
+do
+	# Get its remaining capacity and charge status.
+	capacity=$(cat "$battery"/capacity) || break
+	status=$(sed "s/[Dd]ischarging/-/;s/[Nn]ot charging/NO/;s/[Cc]harging/+/;s/[Uu]nknown/NULL/;s/[Ff]ull/F/" "$battery"/status)
+done
+
+
+status= "$status" "$capacity" "$( date +"%F %R" )"
+
+
+
+	xsetroot -name "$(status | tr "/n" " ")"
 	sleep 1m
 done

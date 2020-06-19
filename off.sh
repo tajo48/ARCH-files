61478310dad0f6f49278dfe2c3b12cf206879733
tajo48 <55502906+tajo48@users.noreply.github.com>
2020-06-19 04:38:32 +0200

    Update off.sh

--- off.sh
+++ off.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-sudo kill -9 $(pgrep -f bar.sh)
-sudo killall dwm
+#[ $(echo -e "No\nYes" | dmenu -i) == "Yes" ] && echo "xd
+
+options="logout\npoweroff\nreboot"
+
+chosen=$(echo -e "$options" | dmenu -i)
+
+case "$chosen" in
+  logout) sudo kill -9 $(pgrep -f bar.sh) && sudo killall dwm && exit;;
+  poweroff) sudo poweroff;;
+  reboot) sudo reboot;;
+esac

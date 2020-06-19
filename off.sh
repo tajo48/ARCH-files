c59d1d69ea2014f71ec4ee3c306a7c4dbdad2f08
tajo48 <55502906+tajo48@users.noreply.github.com>
2020-06-19 23:30:20 +0200

    Update off.sh

--- off.sh
+++ off.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #[ $(echo -e "No\nYes" | dmenu -i) == "Yes" ] && echo "xd
 
-options="poweroff\nxstop\nreboot\nupdate\nupdatelist"
+options="Poweroff\nXstop\nReboot\nUpdate\nPacman"
 
 chosen=$(echo -e "$options" | dmenu -i)
 
 case "$chosen" in
-  xstop) sudo kill -9 $(pgrep -f bar.sh) && sudo killall dwm;;
-  poweroff) sudo poweroff;;
-  reboot) sudo reboot;;
-  update) sudo pacman -Suy --noconfirm  ;;
-  updatelist) chosen=$(sudo pacman -Qu | awk '{print $1}' | dmenu -i) && sudo pacman -S --noconfirm $chosen ;;
+  Xstop) sudo kill -9 $(pgrep -f bar.sh) && sudo killall dwm;;
+  Poweroff) sudo poweroff;;
+  Reboot) sudo reboot;;
+  Update) sudo pacman -Suy --noconfirm  ;;
+  Pacman) chosen=$(sudo pacman -Qu | awk '{print $1}' | dmenu -i) && sudo pacman -S --noconfirm $chosen ;;
 
 esac

9350f3b9e90f0aad5891f66092e55493932dbc6e
tajo48 <55502906+tajo48@users.noreply.github.com>
2020-06-26 23:00:14 +0200

    Update off.sh

--- off.sh
+++ off.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #[ $(echo -e "No\nYes" | dmenu -i) == "Yes" ] && echo "xd
 
-options="Poweroff\nXstop\nReboot\nUpdate\nPacman\nUinstall"
+options="Poweroff\nXstop\nReboot\nUpdate\nPacman\nUinstall\nRefbar"
 
 chosen=$(echo -e "$options" | dmenu -i)
 
@@ -12,5 +12,5 @@ case "$chosen" in
   Update) sudo pacman -Suy --noconfirm  ;;
   Pacman) chosen=$(sudo pacman -Qu | awk '{print $1}' | dmenu -i) && sudo pacman -S --noconfirm $chosen ;;
   Uinstall) chosen=$(sudo pacman -Q | awk '{print $1}' | dmenu -i) && sudo pacman -R --noconfirm $chosen ;;
-
+  Refbar) sudo kill -9 $(pgrep -f bar.sh) && sudo kill -9 $(pgrep -f 20min.sh) && sudo sh ARCH-files/bar.sh & ;;
 esac

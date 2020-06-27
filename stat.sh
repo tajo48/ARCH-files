17696f5763db42fd0dc488283d0742e5efd8b7e2
tajo48 <55502906+tajo48@users.noreply.github.com>
2020-06-27 06:02:53 +0200

    Create stat.sh

---
+++ stat.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+sh /home/tajo48/ARCH-files/20min.sh &
+while :
+do
+statusbat="|"
+
+
+
+if test -r /sys/class/power_supply/BAT0/capacity;
+then
+    for battery in /sys/class/power_supply/BAT?
+    do
+    capacity=$(cat "$battery"/capacity) || break
+    bstatus=$(sed "s/[Dd]ischarging/-/;s/[Nn]ot charging/NO/;s/[Cc]harging/+/;s/[Uu]nknown/NULL/;s/[Ff]ull/F/" "$battery"/status)
+
+statusbat="|"$capacity$bstatus$statusbat
+done
+fi
+
+usedmem="$(vmstat -s | grep "used memory" | awk '{print $1}' )"
+allmem="$(vmstat -s | grep "total memory" | awk '{print $1}' )"
+mem="$( calc "$(echo $usedmem)" / "$(echo $allmem)" *100 | tr "~" " " | tr "." " " | awk '{print $1}' )"
+
+
+status=$mem"%"$statusbat"$(date +"%F %R" )""|""$(pacman -Qu | wc -l)"" of ""$(pacman -Q | wc -l)"
+
+
+
+echo $status
+    sleep 10s
+done

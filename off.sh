7032cfddce7dd3cd0ed990ec10a767b8940b171f
tajo48 <55502906+tajo48@users.noreply.github.com>
2020-06-30 01:42:36 +0200

    Update off.sh

--- off.sh
+++ off.sh
@@ -13,5 +13,6 @@ case "$chosen" in
   Pacman) chosen=$(sudo pacman -Qu | awk '{print $1}' | dmenu -i) && sudo pacman -S --noconfirm $chosen ;;
   Uinstall) chosen=$(sudo pacman -Q | awk '{print $1}' | dmenu -i) && sudo pacman -R --noconfirm $chosen ;;
   Gopoweroff) chosen=$(echo -e "10m\n20m\n30m\n1h\n2h\n3h\n8h" | dmenu -i) && sudo sleep $chosen && sudo poweroff ;;
+  Offgopoweroff) sudo kill -9 $(pgrep -f off.sh) ;;
   Refbar) sudo kill -9 $(pgrep -f bar.sh) && sudo kill -9 $(pgrep -f 20min.sh) && sudo sh ARCH-files/bar.sh & ;;
 esac

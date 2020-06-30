1918e9fd8a0153509da4b807653f38c850987197
tajo48 <55502906+tajo48@users.noreply.github.com>
2020-06-30 05:44:31 +0200

    Update off.sh

--- off.sh
+++ off.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #[ $(echo -e "No\nYes" | dmenu -i) == "Yes" ] && echo "xd
 
-options="Poweroff\nInstall\nXstop\nReboot\nUpdate\nPacman\nUinstall\nRefbar\nGopoweroff\nOffgopoweroff"
+options="Poweroff\nInstall\nXstop\nReboot\nUpdate\nPacman\nRefbar\nGopoweroff\nOffgopoweroff"
 
 chosen=$(echo -e "$options" | dmenu -i)
 
@@ -10,10 +10,13 @@ case "$chosen" in
   Poweroff) sudo poweroff;;
   Reboot) sudo reboot;;
   Update) sudo pacman -Suy --noconfirm  ;;
-  Pacman) chosen=$(sudo pacman -Qu | awk '{print $1}' | dmenu -i) && sudo pacman -S --noconfirm $chosen ;;
-  Install) chosen=$(sudo pacman -Ss | grep "^[A-Za-z]" | tr "/" " " | awk '{ print $2}' | dmenu -i ) && sudo pacman -S --noconfirm $chosen ;;
+  Pacman) update=$(sudo pacman -Qu | awk '{print $1}') && install=$(sudo pacman -Ss | grep "^[A-Za-z]" | tr "/" " " | awk '{ print $2}' ) && chosen=$(echo -e $install"\nProgramy:\n"$update | dmenu -i ) && sudo pacman -S --noconfirm $chosen ;;
   Uinstall) chosen=$(sudo pacman -Q | awk '{print $1}' | dmenu -i) && sudo pacman -R --noconfirm $chosen ;;
   Gopoweroff) chosen=$(echo -e "10m\n20m\n30m\n1h\n2h\n3h\n8h" | dmenu -i) && sudo sleep $chosen && sudo poweroff ;;
   Offgopoweroff) sudo kill -9 $(pgrep -f off.sh) ;;
   Refbar) sudo kill -9 $(pgrep -f bar.sh) && sudo kill -9 $(pgrep -f 20min.sh) && sudo sh ARCH-files/bar.sh & ;;
 esac
+
+
+#update=$(sudo pacman -Qu | awk '{print $1}') && install=$(sudo pacman -Ss | grep "^[A-Za-z]" | tr "/" " " | awk '{ print $2}' ) && chosen=$(echo -e $install"\nProgramy:\n"$update | dmenu -i ) && sudo pacman -S --noconfirm $chosen ;;
+

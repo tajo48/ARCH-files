daa75cf97ed93f0f1f126171ec675e4c0370a6dc
tajo48 <55502906+tajo48@users.noreply.github.com>
2020-06-30 04:02:01 +0200

    Update off.sh

--- off.sh
+++ off.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #[ $(echo -e "No\nYes" | dmenu -i) == "Yes" ] && echo "xd
 
-options="Poweroff\nXstop\nReboot\nUpdate\nPacman\nUinstall\nRefbar\nGopoweroff\nOffgopoweroff"
+options="Poweroff\nInstall\nXstop\nReboot\nUpdate\nPacman\nUinstall\nRefbar\nGopoweroff\nOffgopoweroff"
 
 chosen=$(echo -e "$options" | dmenu -i)
 
@@ -11,6 +11,7 @@ case "$chosen" in
   Reboot) sudo reboot;;
   Update) sudo pacman -Suy --noconfirm  ;;
   Pacman) chosen=$(sudo pacman -Qu | awk '{print $1}' | dmenu -i) && sudo pacman -S --noconfirm $chosen ;;
+  Install) chosen=$(sudo pacman -Ss | grep "^[A-Za-z]" | tr "/" " " | awk '{ print $2}' | dmenu -i ) && sudo pacman -S --noconfirm $chosen ;;
   Uinstall) chosen=$(sudo pacman -Q | awk '{print $1}' | dmenu -i) && sudo pacman -R --noconfirm $chosen ;;
   Gopoweroff) chosen=$(echo -e "10m\n20m\n30m\n1h\n2h\n3h\n8h" | dmenu -i) && sudo sleep $chosen && sudo poweroff ;;
   Offgopoweroff) sudo kill -9 $(pgrep -f off.sh) ;;

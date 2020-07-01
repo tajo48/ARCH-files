45835b2bc7661a5248c1d63e59c11dd454a50c57
tajo48 <55502906+tajo48@users.noreply.github.com>
2020-07-01 04:57:25 +0200

    Update off.sh

--- off.sh
+++ off.sh
@@ -5,7 +5,7 @@ options="Poweroff\nPacman\nSsh"
 chosen=$(echo -e "$options" | dmenu -i)
 
 case "$chosen" in
-  Poweroff) options="Poweroff\nXstop\nReboot\nRefbar\nGopoweroff\nOffgopoweroff" && chosen=$(echo -e "$options" | dmenu -i);;
+  Poweroff) options="Poweroff\nXstop\nReboot\nRefbar" && chosen=$(echo -e "$options" | dmenu -i);;
   Pacman) options="Update\nPacman\nUinstall" && chosen=$(echo -e "$options" | dmenu -i);;
   Ssh) options="Killssh\nStartssh\nDisablessh\nEnablessh" && chosen=$(echo -e "$options" | dmenu -i);;
 esac
@@ -13,13 +13,11 @@ esac
 
 case "$chosen" in
   Xstop) sudo kill -9 $(pgrep -f bar.sh) && sudo kill -9 $(pgrep -f 20min.sh) &&  sudo killall dwm;;
-  Poweroff) sudo poweroff;;
   Reboot) sudo reboot;;
   Update) sudo pacman -Suy --noconfirm  ;;
   Pacman) update=" "$(pacman -Qu | awk '{print $1}') && update=$(echo -e "\n*"$update |  sed 's/ /\n*/g') && install=$(sudo pacman -Ss | grep "^[A-Za-z]" | tr "/" " " | awk '{ print $2}' ) && chosen=$(echo -e $install$update | tr " " "\n" | dmenu -i ) && chosen=$(echo $chosen | tr "\n*" " ") && sudo pacman -S --noconfirm $chosen ;;
   Uinstall) chosen=$(sudo pacman -Q | awk '{print $1}' | dmenu -i) && sudo pacman -R --noconfirm $chosen ;;
-  Gopoweroff) chosen=$(echo -e "10m\n20m\n30m\n1h\n2h\n3h\n8h" | dmenu -i) && sudo sleep $chosen && sudo poweroff ;;
-  Offgopoweroff) sudo kill -9 $(pgrep -f off.sh) ;;
+  Poweroff) chosen=$(echo -e "now\n+10\n+30\n+60\n23:00\n-c" | dmenu -i) && sudo shutdown $chosen ;;
   Refbar) sudo kill -9 $(pgrep -f bar.sh) && sudo kill -9 $(pgrep -f 20min.sh) && sudo sh ARCH-files/bar.sh & ;;
   Killssh) sudo systemctl stop sshd;;
   Startssh) sudo systemctl start sshd;;

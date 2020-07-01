b8827bee7a063108a14d670b246b3ca55e18fcf0
tajo48 <55502906+tajo48@users.noreply.github.com>
2020-07-01 05:32:23 +0200

    Update upgradefiles.sh

--- upgradefiles.sh
+++ upgradefiles.sh
@@ -3,4 +3,3 @@ cd /home/tajo48
 sudo rm -r ARCH-files ARCH-builds
 git clone https://github.com/tajo48/ARCH-files.git
 git clone https://github.com/tajo48/ARCH-builds.git
-sh /home/tajo48/ARCH-files/off.sh

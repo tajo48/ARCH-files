d6e984f82438613ca02ddbc4d09ae6d558e0cf42
tajo48 <55502906+tajo48@users.noreply.github.com>
2020-10-02 15:05:57 +0200

    Update vb.sh

--- vb.sh
+++ vb.sh
@@ -3,5 +3,6 @@ VBoxClient --draganddrop
 VBoxClient --checkhostversion
 VBoxClient --seamless
 VBoxClient --vmsvga
+xrandr --output Virtual-1 --mode 1280x800
 xrandr --output Virtual1 --mode 1280x800
 wal -i /home/tajo48/ARCH-files/photos

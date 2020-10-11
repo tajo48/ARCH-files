4d09932a92338e0d2acfff38554ccae1114ab3ec
tajo48 <55502906+tajo48@users.noreply.github.com>
2020-10-11 11:41:49 +0200

    Update bar.sh

--- bar.sh
+++ bar.sh
@@ -18,8 +18,7 @@ done
 fi
 
 xset r rate 300 50
-xmodmap -e "keycode 9 = Caps_Lock"
-xmodmap -e "keycode 66 = Escape"
+xmodmap -e 'clear Lock' -e 'keycode 0x42 = Escape'
 
 usedmem="$(vmstat -s | grep "used memory" | awk '{print $1}' )"
 allmem="$(vmstat -s | grep "total memory" | awk '{print $1}' )"

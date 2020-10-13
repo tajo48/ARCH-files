bea7fde22f70415749c9e576447283fee06073a5
tajo48 <55502906+tajo48@users.noreply.github.com>
2020-10-13 13:25:50 +0200

    Update upgradefiles.sh

--- upgradefiles.sh
+++ upgradefiles.sh
@@ -5,3 +5,9 @@ git clone https://github.com/tajo48/ARCH-files.git
 git clone https://github.com/tajo48/ARCH-builds.git
 
 echo done | dmenu
+
+
+rm -rf ~/.config/nvim ~/.zshrc ~/.config/coc
+cp ~/ARCH-files/zshrc ~/.zshrc
+mkdir ~/.config/nvim
+cp ~/ARCH-files/init.vim ~/.config/nvim

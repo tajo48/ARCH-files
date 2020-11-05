0b259ccaeb31bdd555c0f59202ce055e1342c0d3
tajo48 <tajo161626@gmail.com>
2020-11-05 10:47:22 +0100

    update

--- off.sh
+++ off.sh
@@ -16,7 +16,7 @@ case "$chosen" in
   Update) sudo pacman -Suy --noconfirm  ;;
   Install) update=" "$(pacman -Qu | awk '{print $1}') && update=$(echo -e "\n*"$update |  sed 's/ /\n*/g') && install=$(sudo pacman -Ss | grep "^[A-Za-z]" | tr "/" " " | awk '{ print $2}' ) && chosen=$(echo -e $install$update | tr " " "\n" | dmenu -i ) && chosen=$(echo $chosen | tr "\n*" " ") && sudo pacman -S --noconfirm $chosen | dmenu ;;
   Uinstall) chosen=$(sudo pacman -Q | awk '{print $1}' | dmenu -i) && sudo pacman -R --noconfirm $chosen ;;
-  Poweroff) chosen=$(echo -e "-c\nnow\n+10\n23:00\n-r now\n-r +10\n-r 23:00" | dmenu -i) && sudo shutdown $chosen ;;
+  Poweroff) chosen=$(echo -e "now\n-r now" | dmenu -i) && sudo shutdown $chosen ;;
   Refbar) sudo kill -9 $(pgrep -f bar.sh) && sudo kill -9 $(pgrep -f 20min.sh) && sudo sh ARCH-files/bar.sh & ;;
   Killssh) sudo systemctl stop sshd;;
   Startssh) sudo systemctl start sshd;;
